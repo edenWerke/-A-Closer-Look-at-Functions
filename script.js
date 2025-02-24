beec3f9d4ad9f457f5b556606ac5220f7226cec7
edenWerke <edenw6292@gmail.com>
2025-02-24 22:32:25 +0300

    Closure

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 'use strict';
 
 
-const  runOnce=function(){
-    console.log('this will never run again')
+// const  runOnce=function(){
+//     console.log('this will never run again')
+// };
+// runOnce();
+// (function(){
+//     console.log('this will never run again'); 
+//     const isPrivate=9;
+//     var notPrivate=8;
+
+// }())  ;
+// (()=>console.log('this will also never run again'))();
+// //    console.log(isPrivate)
+//    console.log(notPrivate)
+// this is no longer active
+const secureBooking=function(){
+    let passengerCount=0;
+// this can access the passangers count eventho the secureBooking finishs excuting 
+    return function(){
+        passengerCount ++;
+        console.log(`${passengerCount} passengers`)
+    };
 };
-runOnce();
-(function(){
-    console.log('this will never run again')
-}())  ;
+//closure remebers a function at its birth place(securebook)
+// closure tay ygebr meslikum var   
+// closure makes connection with function even tho it loses its existance
+// like a person who does not loose its connectivity with its hometown
+// person=function
+//home=parent scope
+// function is like person and closure is like backpack
+// closure are not tangable
+//we do not have to create closure manually
+const booker=secureBooking();
+booker();
+booker();
+booker(); 
+console.dir(booker)
